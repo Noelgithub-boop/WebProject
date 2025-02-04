c48f451fa036cb3c22c4c2de749a552f267ea070
NevehLucky <cnavarroca@cifpfbmoll.eu>
2025-02-04 19:53:30 +0100

    Menu logIn acabado. 'logo a modificar'

---
+++ js/Proyecto.js
@@ -0,0 +1,9 @@
+function logIn() {
+    var login = document.getElementById("logBox");
+    login.style.display = "flex";
+}
+
+function closeLogIn() {
+    var login = document.getElementById("logBox");
+    login.style.display = "none";
+}
