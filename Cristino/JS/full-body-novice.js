6307c3d59402d64b9cdf1563571e5a9d9ba788a4
NevehLucky <cnavarroca@cifpfbmoll.eu>
2025-02-16 23:27:10 +0100

    Página específica para cada entrenamiento

---
+++ Cristino/JS/full-body-novice.js
@@ -0,0 +1,51 @@
+function showStep() {
+    var step = document.getElementById("pasos1");
+    var videos = document.getElementById("videos1");
+    
+    if (step.style.display == "none") {
+        step.style.display = "flex";
+        videos.style.display = "flex";
+    } else {
+        step.style.display = "none";
+        videos.style.display = "none";
+    }
+}
+
+function showStep1() {
+    var step = document.getElementById("pasos2");
+    var videos = document.getElementById("videos2");
+    
+    if (step.style.display == "none") {
+        step.style.display = "flex";
+        videos.style.display = "flex";
+    } else {
+        step.style.display = "none";
+        videos.style.display = "none";
+    }
+}
+
+function showStep2() {
+    var step = document.getElementById("pasos3");
+    var videos = document.getElementById("videos3");
+    
+    if (step.style.display == "none") {
+        step.style.display = "flex";
+        videos.style.display = "flex";
+    } else {
+        step.style.display = "none";
+        videos.style.display = "none";
+    }
+}
+
+function showStep3() {
+    var step = document.getElementById("pasos4");
+    var videos = document.getElementById("videos4");
+    
+    if (step.style.display == "none") {
+        step.style.display = "flex";
+        videos.style.display = "flex";
+    } else {
+        step.style.display = "none";
+        videos.style.display = "none";
+    }
+}
